26b9b26540a9f023fc418dc7f0c80e8ac7d6f162
Nik <kolonkovn@gmail.com>
2024-10-06 23:43:12 +0500

    Adding JSON

--- go/Parser/parser.go
+++ go/Parser/parser.go
@@ -1,13 +1,43 @@
 package Parser
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+type WeatherSelectors struct {
+	Name        string `json:"name"`
+	Temperature string `json:"temperature"`
+	Update      string `json:"update"`
+}
+type WeatherData struct {
+	Tu_day WeatherSelectors `json:"tu_day"`
+}
+
 func Parse(resp *http.Response) {
+
+	file, err := os.Open("json/HTML.json")
+	if err != nil {
+		log.Fatal("Ошибка при открытии файла:", err)
+	}
+	defer file.Close()
+
+	// Декодируем JSON в структуру
+	var weatherData WeatherData
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&weatherData)
+	if err != nil {
+		log.Fatal("Ошибка при декодировании JSON:", err)
+	}
+
+	// Закрываем тело ответа после обработки
+	defer resp.Body.Close()
+
 	defer resp.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
@@ -15,21 +45,21 @@ func Parse(resp *http.Response) {
 		log.Fatal(err)
 	}
 
-	cityElem := doc.Find(".hdr__inner")
+	cityElem := doc.Find(weatherData.Tu_day.Name)
 	if cityElem.Length() > 0 {
 		fmt.Printf("%s\n", cityElem.Text())
 	} else {
 		fmt.Println("Не удалось найти название района.")
 	}
 
-	temperatureElem := doc.Find(".p-forecast__temperature-value")
+	temperatureElem := doc.Find(weatherData.Tu_day.Temperature)
 	if temperatureElem.Length() > 0 {
 		fmt.Printf("Температура: %s \n", temperatureElem.Text())
 	} else {
 		fmt.Println("Не удалось найти температуру.")
 	}
 
-	lastUpdateElem := doc.Find(".p-forecast__title")
+	lastUpdateElem := doc.Find(weatherData.Tu_day.Update)
 	if lastUpdateElem.Length() > 0 {
 		fmt.Printf("Последнее обновление: %s\n", lastUpdateElem.Text())
 	} else {
